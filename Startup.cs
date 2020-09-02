856bac830a72d6e8873ab3b14d9bd49d183ef766
romuloleitao <romulojleitao@gmail.com>
2020-09-02 15:32:24 -0400

    Adding EF Core structure, connectionstring and database.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,8 +17,12 @@ namespace Commander
 
         public IConfiguration Configuration { get; }
 
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+                (Configuration.GetConnectionString("CommanderConnection")));
+
             services.AddControllers();
 
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
